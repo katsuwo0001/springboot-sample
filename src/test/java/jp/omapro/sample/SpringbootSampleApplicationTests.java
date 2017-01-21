d92c857c3f0cef63244225f5502bee1b1ebe9877
iwata_katsuhiro <katsuwo.com@gmail.com>
2017-01-22 08:55:33 +0900

    first commit

---
+++ src/test/java/jp/omapro/sample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,18 @@
+package jp.omapro.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		//assertEquals(expected, actual);
+
+	}
+
+}
